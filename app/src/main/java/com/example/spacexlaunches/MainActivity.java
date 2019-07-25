1a68f881fdb67d98535818e2165acfd1bba8ec13
nelsongoncalves <nelsonfcgoncalves@gmail.com>
2019-07-25 16:02:49 +0100

    Implementing fixes to errors

--- app/src/main/java/com/example/spacexlaunches/MainActivity.java
+++ app/src/main/java/com/example/spacexlaunches/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.spacexlaunches;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
@@ -15,5 +19,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AppCenter.start(getApplication(),"SpaceX Launches", Analytics.class, Crashes.class);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new UpFragment()).commit();
     }
+    //react to click on bottom navigation buttons
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_up:
+                            selectedFragment = new UpFragment();
+                            break;
+                        case R.id.nav_down:
+                            selectedFragment = new DownFragment();
+                            break;
+                        case R.id.nav_left:
+                            selectedFragment = new LeftFragment();
+                            break;
+                        case R.id.nav_right:
+                            selectedFragment = new RightFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
